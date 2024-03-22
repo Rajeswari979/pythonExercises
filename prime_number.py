afc60c5340b24925ff6f74e4c8e663218e96dab2
Rajeswari979 <161428245+Rajeswari979@users.noreply.github.com>
2024-03-22 11:29:50 +0530

    Update prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -1,5 +1,4 @@
 
-
 status=0
 for i in [23,45,78,71,83,4,6,78]:
     status=0
@@ -10,8 +9,9 @@ for i in [23,45,78,71,83,4,6,78]:
             if i%j==0:
                 status=1
 
-        if status != 1:
+        if status != 1 :
             print(f"{i} is a prime number")    
-        
+        else:
+            print(f'{i} is a not prime number')
 
             
