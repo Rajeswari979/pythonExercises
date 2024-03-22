2ab2e21a9376156a0743e0ee44630ce025fda240
Rajeswari979 <161428245+Rajeswari979@users.noreply.github.com>
2024-03-22 10:59:42 +0530

    Update frequency_checks.py

--- frequency_checks.py
+++ frequency_checks.py
@@ -1,12 +1,9 @@
 list1=[1,2,3,1,4,5,2,1,3,1,6,6]
 count=0
-element=0
-for i in range(0,len(list1)):
-    count=0
-    for j in range(0,len(list1)):
-        if list1[i] is list1[j]:
-            count+=1
-            element=list1[i]
-print(f"{list1[i]} is {count} times repeated")     
+element=int(input("enter the element to check : "))
 
- 
+for j in list1:
+    if j == element:
+        count+=1
+
+print(f"{element} is {count} times repeated")     
